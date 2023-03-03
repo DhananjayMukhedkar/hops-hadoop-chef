b9c3bdcdf65297e671d33dcd79f10d5480e9a44d
Dhananjay Mukhedkar <dhananjay@logicalclocks.com>
2023-03-03 09:44:35 +0100

    update tar.gz extension to tgz

--- attributes/default.rb
+++ attributes/default.rb
@@ -559,4 +559,4 @@ default['hops']['docker']['testconnector']['image']['name']    = "testconnector"
 default['hops']['docker']['testconnector']['image']['version'] = "0.1"
 #default['hops']['docker']['testconnector']['download_url'] = 
 # "#{node['download_url']}/testconnector/#{node['hops']['docker']['testconnector']['image']['version']}/testconnector.tgz"
-default['hops']['docker']['testconnector']['download_url'] = "https://repo.hops.works/dev/dhananjay/testconnector/#{node['hops']['docker']['testconnector']['image']['version']}/testconnector.tar.gz"
+default['hops']['docker']['testconnector']['download_url'] = "https://repo.hops.works/dev/dhananjay/testconnector/#{node['hops']['docker']['testconnector']['image']['version']}/testconnector.tgz"
